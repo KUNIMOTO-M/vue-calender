59831452f70934ff800b08bbe993ed91bb2aca13
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-07-30 14:32:35 +0900

    4-2終了

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,15 +1,8 @@
 <template>
 <div>
-  <h1 class="text-h1">Calender</h1>
-  <v-list>
-    <v-list-item v-for="event in events" :key="event.id">
-      {{ event.name }}
-    </v-list-item>  
-  </v-list>
-  <p>events:</p>
-  <p>{{ events }}</p>
-  <v-btn type="submit" v-on:click="fetchEvents">Fetchevents</v-btn>
-
+  <v-sheet height="100vh">
+    <v-calendar></v-calendar>
+  </v-sheet>
 </div>
 </template>
 
